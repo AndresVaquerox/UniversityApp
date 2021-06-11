c0884d29ac688ed5512e4038e929112ae7c61840
Avaquerox <andresvaquerox@hotmail.es>
2021-06-10 21:01:02 -0500

    Desarrollo del University App Complet

---
+++ University.App/University.App/Helpers/Languages.cs
@@ -0,0 +1,32 @@
+﻿using University.App.Interfaces;
+using University.App.Resources;
+using Xamarin.Forms;
+
+namespace University.App.Helpers
+{
+    public static class Languages
+    {
+        static Languages()
+        {
+            var ci = DependencyService.Get<ILocalize>().GetCurrentCultureInfo();
+            Resource.Culture = ci;
+
+
+            DependencyService.Get<ILocalize>().SetLocale(ci);
+        }
+
+        public static string Accept { get { return Resource.Accept; } }
+        public static string Notification { get { return Resource.Notification; } }
+        public static string NoInternetConnection { get { return Resource.NoInternetConnection; } }
+        public static string FieldsRequired { get { return Resource.FieldsRequired; } }
+        public static string RegisterSuccessfull { get { return Resource.RegisterSuccessfull; } }
+        public static string ChangePasswordSuccessfully { get { return Resource.ChangePasswordSuccessfully; } }
+        public static string ProcessSuccessfull { get { return Resource.ProcessSuccessfull; } }
+        public static string Bad { get { return Resource.Bad; } }
+        public static string Well { get { return Resource.Well; } }
+        public static string Acceptable { get { return Resource.Acceptable; } }
+        public static string Excellent { get { return Resource.Excellent; } }
+   
+    
+    }
+}
---
+++ University.App/University.App/ViewModels/Forms/PQRSViewModel.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using University.App.Helpers;
+
+namespace University.App.ViewModels.Forms
+{
+    public class PQRSViewModel : BaseViewModel
+    {
+        #region Attributes
+        public class TypeRequest
+        {
+            public string Name { get; set; }
+        }
+        public class RateService
+        {
+            public string Name { get; set; }
+        }
+
+
+        private List<TypeRequest> _typeRequests;
+        private List<RateService> _rateServices;
+        #endregion
+
+
+
+        #region Properties
+        public List<TypeRequest> TypeRequests
+        {
+            get { return this._typeRequests; }
+            set { this.SetValue(ref this._typeRequests, value); }
+        }
+        public List<RateService> RateServices
+        {
+            get { return this._rateServices; }
+            set { this.SetValue(ref this._rateServices, value); }
+        }
+        #endregion
+
+
+
+        #region Constructor
+        public PQRSViewModel()
+        {
+            this.LoadTypeRequests();
+            this.LoadRateServices();
+        }
+        #endregion
+        #region Methods
+        private void LoadTypeRequests()
+        {
+            this.TypeRequests = new List<TypeRequest>
+            {
+                new TypeRequest { Name = "Petition" },
+                new TypeRequest { Name = "Complain" },
+                new TypeRequest { Name = "Claim" },
+                new TypeRequest { Name = "Suggestion" }
+            };
+        }
+        private void LoadRateServices()
+        {
+            this.RateServices = new List<RateService>
+            {
+                new RateService { Name = Languages.Bad },
+                new RateService { Name = "Regular" },
+                new RateService { Name = Languages.Well },
+                new RateService { Name = Languages.Acceptable },
+                new RateService { Name = Languages.Excellent }
+            };
+        }
+        #endregion
+    }
+}
